2f7c5bd5e4601d6082bf494ea27e557a215f998b
Bishwajit Samanta <bishwajitsamanta386@gmail.com>
2023-01-29 01:38:24 +0530

    initial commit

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    const connectionParms = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    try {
+        mongoose.connect(process.env.DB, connectionParms);
+        console.log('Connected to database');
+    } catch (error) {
+        console.log(error);
+    }
+};
